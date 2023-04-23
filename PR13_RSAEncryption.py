c5b41d063c46a5d9fba6f6564bdddcfb1d8ec2e0
William M <artofmediel@gmail.com>
2023-04-24 06:40:41 +0800

    Add files via upload

---
+++ PR13_RSAEncryption.py
@@ -0,0 +1,37 @@
+import rsa
+
+# generate keys with length of 1024 bits
+public_key, private_key = rsa.newkeys(1024)
+
+# generate public and private keys
+#with open("public.pem", "rb") as f:
+#    f.write(public_key.save_pkcs1("PEM"))
+
+#with open("private.pem", "wb") as f:
+#    f.write(private_key.save_pkcs1("PEM"))
+
+# using the generated keys
+with open("public.pem", "rb") as f:
+    public_key = rsa.PublicKey.load_pkcs1(f.read())
+
+with open("private.pem", "rb") as f:
+    private_key = rsa.PrivateKey.load_pkcs1(f.read())
+
+# Sample encryption:
+message = "Hello, my password is testpassword123"
+
+# encrypt the message with the public key
+#encrypted_message = rsa.encrypt(message.encode(), public_key)
+# check
+#print(encrypted_message)
+
+# make a file : encrypted.message
+#with open("encrypted.message", "wb") as f:
+#    f.write(encrypted_message)
+
+# open encrypted file
+encrypted_message = open("encrypted.message", "rb").read()
+# decrypt using private key
+clear_message = rsa.decrypt(encrypted_message, private_key)
+# check
+print(clear_message.decode())
